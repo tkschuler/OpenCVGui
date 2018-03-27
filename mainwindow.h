002c7fb678eb28df15439442bf1e4f2d4dfdb5e3
Tristan Schuler <tkschuler@yahoo.com>
2018-03-27 15:31:04 -0500

    Initial commit

---
+++ mainwindow.h
@@ -0,0 +1,47 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QTimer>
+#include <opencv2/opencv.hpp>
+//#include <opencv2/highgui.hpp>
+//#include <opencv2/imgproc.hpp>
+using namespace cv;
+
+#include <iostream>
+using namespace std;
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private slots:
+    void on_pushButton_open_webcam_clicked();
+
+    void on_pushButton_close_webcam_clicked();
+
+    void update_window();
+
+    void on_pushButton_crosshair_clicked();
+
+private:
+    Ui::MainWindow *ui;
+
+    QTimer *timer;
+    VideoCapture cap;
+
+    Mat frame;
+    QImage qt_image;
+
+    bool crosshair;
+};
+
+#endif // MAINWINDOW_H
